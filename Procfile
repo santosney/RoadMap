d92d5d2b9df6f40c5c9456cceea5904693f62dc5
santosney <robertocarlosdosantos9@gmail.com>
2022-01-24 09:13:46 +0100

    initial commit & heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web : gunicorn gitsaf.wsgi --log-file -
---
+++ saf/url.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from rest_framework import routers
+urlpatterns = [
+ 
+    
+    path('listuser/', views.listuser),
+    path('addUser/', views.addUser),
+    path('listpublication/', views.lis_publication),
+    path('addPublication/', views.addPublication),
+    path('UpdateUser/<int:id>/', views.UpdateUser),
+    path('UpdatePublication/<int:id>/', views.UpdatePublication),
+    
+]
