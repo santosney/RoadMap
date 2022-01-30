c6ec30e99a67fee883c9b4529887d1f686d9feda
santosney <robertocarlosdosantos9@gmail.com>
2022-01-30 10:17:47 +0100

    prod

--- gitsaf/prod_seeting.py
+++ gitsaf/prod_seeting.py
@@ -2,7 +2,7 @@ import dj_database_url
 import dj_database_url
 from gitsaf.settings import *
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = 'django-insecure-*^^^0k27*u3#%fry@abb1c#ra$40i@2^g1=tgeqg_vn48k%+4p'
 
 DATABASES['default'].update(dj_database_url.config(conn_max_age=600))
