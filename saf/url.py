c0b952c64e49a1148da3fec6e8715eabe791412b
santosney <robertocarlosdosantos9@gmail.com>
2022-02-04 23:45:05 +0100

    next

--- saf/url.py
+++ saf/url.py
@@ -4,14 +4,14 @@ from rest_framework import routers
 urlpatterns = [
  
     
-    path('listuser/', views.listuser),
-    path('addUser/', views.addUser),
-    path('listpublication/', views.lis_publication),
-    path('addPublication/', views.addPublication),
-    path('UpdateUser/', views.UpdateUser),
-    path('UpdatePublication/', views.UpdatePublication),
-    path('login/', views.login),
-    path('delete_user/', views.delete_user),
-    path('deletePublication/', views.deletePublication)
+    path('listuser', views.listuser),
+    path('addUser', views.addUser),
+    path('listpublication', views.lis_publication),
+    path('addPublication', views.addPublication),
+    path('UpdateUser', views.UpdateUser),
+    path('UpdatePublication', views.UpdatePublication),
+    path('login', views.login),
+    path('delete_user', views.delete_user),
+    path('deletePublication', views.deletePublication)
     
 ]
