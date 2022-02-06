70c2f8218731f78a4c57d14774a1d3fcf28b4f09
santosney <robertocarlosdosantos9@gmail.com>
2022-02-06 08:07:48 +0100

    next commit

--- gitsaf/settings.py
+++ gitsaf/settings.py
@@ -112,19 +112,34 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-CROS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#     '*'
+# )
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
 
-CORS_ALLOW_HEADERS  =  [ 
-    "accept" , 
-    "accept-encoding" , 
-    "authorization" , 
-    "content-type" , 
-    "dnt" , 
-    "origin" , 
-    "user-agent" , 
-    "x-csrftoken" , 
-    "x-requested-with" , 
-]
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
